0afffd6f2e85554b169d063135b559bfc5ef89ac
Seo_Jun_ <117076847+BeigeJun@users.noreply.github.com>
2023-08-30 21:21:14 +0900

    Update MFCDOC.h

--- MFC/OpenCV/MFCDOC.h
+++ MFC/OpenCV/MFCDOC.h
@@ -6,6 +6,7 @@
 #pragma once
 #include <atlimage.h>
 #include "SLD.h"
+
 class CMFCGAJADoc : public CDocument
 {
 protected: // serialization에서만 만들어집니다.
@@ -41,18 +42,42 @@ protected:
 protected:
 	DECLARE_MESSAGE_MAP()
 public:
+	int mode;
+	int start_x, start_y, end_x, end_y;
+	bool ZOOM_flag;
+	bool money_flag;
+	double average;
+	int HISTO_arr[256];
+	int HISTO_R[256];
+	int HISTO_G[256];
+	int HISTO_B[256];
 	CImage m_Img;
 	CImage Second_Img;
-	HBITMAP bitmap;
-	BYTE** GRAYIMG;
-	BYTE** BINARYIMG;
-	BYTE** SOBELIMG;
+	CImage Pro_w;
+	CImage Pro_h;
+	CImage HISTO_Img;
+	CImage HISTO_str_Img;
+	CImage HISTO_R_Img;
+	CImage HISTO_G_Img;
+	CImage HISTO_B_Img;
+	CImage MONEY;
+	int** GRAYIMG;
+	int** BINARYIMG;
+	int** SOBELIMG;
 	void CMFCGAJADoc::SetPixel(int x, int y, BYTE color, CImage * image);
 	void CMFCGAJADoc::OnFileOpen();
 	void CMFCGAJADoc::GRAY();
+	void CMFCGAJADoc::MAKE_GRAY();
 	void CMFCGAJADoc::Binary();
 	void CMFCGAJADoc::Slider();
 	void CMFCGAJADoc::Sobel();
+	void CMFCGAJADoc::PROJECTION(int select);
+	void CMFCGAJADoc::HISTO();
+	void CMFCGAJADoc::HISTO_streching();
+	void CMFCGAJADoc::HISTO_equalization();
+	void CMFCGAJADoc::HISTO_RGB();
+	void CMFCGAJADoc::ZOOM();
+	void CMFCGAJADoc::FIND();
 #ifdef SHARED_HANDLERS
 	// 검색 처리기에 대한 검색 콘텐츠를 설정하는 도우미 함수
 	void SetSearchContent(const CString& value);
