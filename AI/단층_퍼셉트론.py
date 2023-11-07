2029c8c7fc334272d4ebb4d354d64859c8dea479
Seo_Jun_ <117076847+BeigeJun@users.noreply.github.com>
2023-11-07 17:55:35 +0900

    Create 단층_퍼셉트론.py

---
+++ AI/단층_퍼셉트론.py
@@ -0,0 +1,9 @@
+import numpy as np
+num = 10
+tall = [150,156,163,165,168,170,182,183,187,189]
+weight = [35,45,50,54,70,63,75,77,85,90]
+Y = np.array(tall)
+X = np.array(weight)
+
+Weight = 0.0 #가중치
+Bais = 0.0 #편향
