1264c7ca0e899cea2161b4c85fecb0fbc99528e3
Seo_Jun_ <117076847+BeigeJun@users.noreply.github.com>
2024-01-10 15:59:49 +0900

    Update 7. 알파벳 구별하기 학습 파일.py

--- AI/7. 알파벳 구별하기 학습 파일.py
+++ AI/7. 알파벳 구별하기 학습 파일.py
@@ -21,8 +21,9 @@ input_data = [[1.0, 1.0, 1.0, 1.0, 1.0,
                1.0, 0.1, 0.1, 0.1, 0.1,
                1.0, 0.1, 0.1, 0.1, 1.0]]
 
-out_1 = [0.0] * 15  # 은닉층 1번째 15개
-out_2 = [0.0] * 7  # 은닉층 2번째 7개
+out_1 = [0.0] * 15
+out_2 = [0.0] * 7
+output = [0.0, 0.0, 0.0]
 weight_hid2_to_out = np.random.uniform(low=-0.5, high=0.5, size=(3, 7))
 weight_hid1_to_hid2 = np.random.uniform(low=-0.5, high=0.5, size=(7, 15))
 weight_in_to_hid1 = np.random.uniform(low=-0.5, high=0.5, size=(15, 25))
@@ -30,12 +31,11 @@ neurons_in_layers = [25, 15, 7, 3]
 biases = [2 * np.random.rand(neurons, 1) - 1 for neurons in neurons_in_layers[1:]]
 #가중치 초기화 할 때는 -0.5 ~ 0.5로하자
 target = [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
-output = [0.0, 0.0, 0.0]
 bias = 1.0
 error = 0.0
 total_error = 0.0
-lrate = 0.05
-epochs = 20000
+lrate = 0.01
+epochs = 100000
 
 def weight_write(epoch,total_error, w1, w2, w3, b):
     f = open("C:/Users/wns20/PycharmProjects/pythonProject10/weight.txt", 'w')
@@ -57,25 +57,16 @@ def weight_write(epoch,total_error, w1, w2, w3, b):
             write += "%.1f " % biases[i][j]
     f.write(write)
     f.close()
-
+def forward_pass(data,w,b,bias_num,out):
+    for i in range(len(w)):
+        for number in range(len(w[i])):
+            out[i] += w[i][number] * data[number]
+        out[i] = sigmoid(out[i] + b[bias_num][i])
 def Forward_pass(data, w1, w2, w3, b):
-
-    for i in range(len(w1)):  # 15개의 델타값을 만들어야하니까 15번
-        for number in range(len(w1[i])):  # 가중치랑 인풋값 곱학기 25번
-            out_1[i] += w1[i][number] * data[number]
-        out_1[i] = sigmoid(out_1[i] + b[0][i])
-    # ---------------------입력층 - > 1차 은닉층--------------------------------------------
-    for i in range(len(w2)):  # 7개의 델타값을 만들어야 하니까 7번
-        for number in range(len(w2[i])):  # 15번의 곱셈을 진행
-            out_2[i] += w2[i][number] * data[number]
-        out_2[i] = sigmoid(out_2[i] + b[1][i])
-    # ---------------------1차 은닉층 - > 2차 은닉층----------------------------------------
-    for i in range(len(w3)):  # 3개의 출력을 만들어야 하니까 3번
-        for number in range(len(w3[i])):  # 7번의 곱셈을 진행
-            output[i] += w3[i][number] * out_2[number]
-        output[i] = sigmoid(output[i] + b[2][i])
-    # ---------------------2차 은닉층 - > 출력층--------------------------------------------
-
+    forward_pass(data, w1, b, 0, out_1)
+    forward_pass(data, w2, b, 1, out_2)
+    forward_pass(data, w3, b, 2, output)
+    return out_1 , out_2 , output
 def Backward_pass(data, target, w1, w2, w3, b, out_1, out_2, output, lrate):
     delta_1 = np.zeros(len(w3))
     delta_2 = np.zeros(len(w2))
@@ -114,6 +105,7 @@ def Backward_pass(data, target, w1, w2, w3, b, out_1, out_2, output, lrate):
             w1[i][number] += lrate * delta_3[i] * data[number]
 
         b[0][i] += lrate * delta_3[i]
+        return w1,w2,w3,b
 def train(input_data, target_data, w1, w2, w3, b, lrate, epochs):
     minimun_error = 10.0
     comparison_error = 10.0
@@ -121,7 +113,7 @@ def train(input_data, target_data, w1, w2, w3, b, lrate, epochs):
         total_error = 0.0
         for i in range(len(input_data)):
             #순전파
-            Forward_pass(input_data[i], w1, w2, w3, b)
+            out_1, out_2, output = Forward_pass(input_data[i], w1, w2, w3, b)
             #에너지 구하기
             error = 0.0
             for j in range(len(target_data[i])):
