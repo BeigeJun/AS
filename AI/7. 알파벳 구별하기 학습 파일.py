71ac136f6fa6aa14614799d28a53f06a71ba21b4
Seo_Jun_ <117076847+BeigeJun@users.noreply.github.com>
2024-01-12 01:01:17 +0900

    Update 7. 알파벳 구별하기 학습 파일.py

--- AI/7. 알파벳 구별하기 학습 파일.py
+++ AI/7. 알파벳 구별하기 학습 파일.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 def sigmoid(data):
     return 1.0 / (1.0 + np.exp(-data))
 
@@ -37,7 +36,7 @@ total_error = 0.0
 lrate = 0.05
 epochs = 100000
 
-def weight_write(epoch,total_error, w1, w2, w3, b):
+def weight_write(total_error, w1, w2, w3):
     f = open("C:/Users/wns20/PycharmProjects/pythonProject10/weight.txt", 'w')
     write = "error : %f \n w1 : " % total_error
     for i in range(len(w1)):
@@ -77,22 +76,19 @@ def delta_rule(weight1,weight2,b,b_num,delta_1,delta_2,lrate,out_1,out_2):
             weight1[i][number] += lrate * delta_2[i] * out_1[number]
         b[b_num][i] += lrate * delta_2[i]
 
+def first_delta_rule(weight,b,b_num,delta, target, output):
+    for i in range(len(weight)): #3번 반복
+        first_error = target[i] - output[i] #출력층 에러 구하기
+        delta[i] = first_error * output[i] * (1 - output[i]) #델타값 구하기
+        for j in range(len(weight[i])): #7번 반복
+            weight[i][j] += lrate * delta[i] * out_2[j]
+        b[b_num][i] += lrate * delta[i]
 def Backward_pass(data, target, w1, w2, w3, b, out_1, out_2, output, lrate):
     delta_1 = np.zeros(len(w3))
     delta_2 = np.zeros(len(w2))
     delta_3 = np.zeros(len(w1))
-    # --------------출력층에서 에러 구하고 그 밑 은닉층_2을 이어주는 가중치 업데이트-----------------
-    for i in range(len(w3)): #3번 반복
-        first_error = target[i] - output[i] #출력층 에러 구하기
-        delta_1[i] = first_error * output[i] * (1 - output[i]) #델타값 구하기
-
-        for j in range(len(w3[i])): #7번 반복
-            w3[i][j] += lrate * delta_1[i] * out_2[j]
-
-        b[2][i] += lrate * delta_1[i]
-    # ----------------------은닉층_2 -> 은닉층_1 로 가는 가중치 없데이트 -----------------------
-    delta_rule(w2, w3, b, 1, delta_1, delta_2,lrate,out_1,out_2)
-    # ---------------------은닉층_1 -> 입력층 가중치 업데이트---------------------------------
+    first_delta_rule(w3, b, 2, delta_1, target, output)
+    delta_rule(w2, w3, b, 1, delta_1, delta_2, lrate, out_1, out_2)
     delta_rule(w1, w2, b, 0, delta_2, delta_3, lrate, data, out_1)
 
 def train(input_data, target_data, w1, w2, w3, b, lrate, epochs):
@@ -117,6 +113,5 @@ def train(input_data, target_data, w1, w2, w3, b, lrate, epochs):
         #에러가 전에 나온 에러보다 제일 작을 때만 텍스트 파일에 저장하기
         if(minimun_error > comparison_error):
             minimun_error = comparison_error
-            weight_write(epoch,total_error, w1, w2, w3, b)
-# 예시 호출
+            weight_write(total_error, w1, w2, w3)
 train(input_data, target, weight_in_to_hid1, weight_hid1_to_hid2, weight_hid2_to_out, biases, lrate, epochs)
