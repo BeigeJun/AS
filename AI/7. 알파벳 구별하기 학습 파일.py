ffb0fad67f8378fdd0e30b9b8695b81059f9eda3
Seo_Jun_ <117076847+BeigeJun@users.noreply.github.com>
2024-01-16 12:10:57 +0900

    Update 7. 알파벳 구별하기 학습 파일.py

--- AI/7. 알파벳 구별하기 학습 파일.py
+++ AI/7. 알파벳 구별하기 학습 파일.py
@@ -20,12 +20,9 @@ input_data = [[1.0, 1.0, 1.0, 1.0, 1.0,
                1.0, 0.1, 0.1, 0.1, 0.1,
                1.0, 0.1, 0.1, 0.1, 1.0]]
 
-out_1 = [0.0] * 15
-out_2 = [0.0] * 7
-output = [0.0, 0.0, 0.0]
-weight_hid2_to_out = np.random.uniform(low=-0.5, high=0.5, size=(3, 7))
-weight_hid1_to_hid2 = np.random.uniform(low=-0.5, high=0.5, size=(7, 15))
-weight_in_to_hid1 = np.random.uniform(low=-0.5, high=0.5, size=(15, 25))
+weight_hid2_to_out = np.random.uniform(low=-5.0, high=5.0, size=(3, 7))
+weight_hid1_to_hid2 = np.random.uniform(low=-5.0, high=5.0, size=(7, 15))
+weight_in_to_hid1 = np.random.uniform(low=-5.0, high=5.0, size=(15, 25))
 neurons_in_layers = [25, 15, 7, 3]
 biases = [2 * np.random.rand(neurons, 1) - 1 for neurons in neurons_in_layers[1:]]
 #가중치 초기화 할 때는 -0.5 ~ 0.5로하자
@@ -33,7 +30,7 @@ target = [[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]
 bias = 1.0
 error = 0.0
 total_error = 0.0
-lrate = 0.05
+lrate = 0.1
 epochs = 100000
 
 def weight_write(total_error, w1, w2, w3):
@@ -56,17 +53,20 @@ def weight_write(total_error, w1, w2, w3):
             write += "%.1f " % biases[i][j]
     f.write(write)
     f.close()
-def forward_pass(data,w,b,bias_num,out):
+def forward_pass(data, w, b, bias_num, out):
     for i in range(len(w)):
         for number in range(len(w[i])):
             out[i] += w[i][number] * data[number]
         out[i] = sigmoid(out[i] + b[bias_num][i])
+    return out
+
 def Forward_pass(data, w1, w2, w3, b):
-    forward_pass(data, w1, b, 0, out_1)
-    forward_pass(data, w2, b, 1, out_2)
-    forward_pass(data, w3, b, 2, output)
-    return out_1 , out_2 , output
-def delta_rule(weight1,weight2,b,b_num,delta_1,delta_2,lrate,out_1,out_2):
+    out_1 = forward_pass(data, w1, b, 0, [0.0] * 15)
+    out_2 = forward_pass(data, w2, b, 1, [0.0] * 7)
+    output = forward_pass(data, w3, b, 2, [0.0, 0.0, 0.0])
+    return out_1, out_2, output
+
+def delta_rule(weight1, weight2, b, b_num, delta_1, delta_2, lrate, out_1, out_2):
     error = 0
     for i in range(len(weight1)):
         for j in range(len(weight2)):
@@ -75,43 +75,43 @@ def delta_rule(weight1,weight2,b,b_num,delta_1,delta_2,lrate,out_1,out_2):
         for number in range(len(weight1[i])):
             weight1[i][number] += lrate * delta_2[i] * out_1[number]
         b[b_num][i] += lrate * delta_2[i]
+    return delta_2, weight1
 
-def first_delta_rule(weight,b,b_num,delta, target, output):
-    for i in range(len(weight)): #3번 반복
-        first_error = target[i] - output[i] #출력층 에러 구하기
-        delta[i] = first_error * output[i] * (1 - output[i]) #델타값 구하기
-        for j in range(len(weight[i])): #7번 반복
+def first_delta_rule(weight, b, b_num, delta, target, output, out_2):
+    for i in range(len(weight)):
+        first_error = target[i] - output[i]
+        delta[i] = first_error * output[i] * (1 - output[i])
+        for j in range(len(weight[i])):
             weight[i][j] += lrate * delta[i] * out_2[j]
         b[b_num][i] += lrate * delta[i]
-def Backward_pass(data, target, w1, w2, w3, b, out_1, out_2, output, lrate):
+    return delta, weight
+
+def Backward_pass(data, target, w1, w2, w3, b, out1, out2, out_put, lrate):
     delta_1 = np.zeros(len(w3))
     delta_2 = np.zeros(len(w2))
     delta_3 = np.zeros(len(w1))
-    first_delta_rule(w3, b, 2, delta_1, target, output)
-    delta_rule(w2, w3, b, 1, delta_1, delta_2, lrate, out_1, out_2)
-    delta_rule(w1, w2, b, 0, delta_2, delta_3, lrate, data, out_1)
+    delta_1, w3 = first_delta_rule(w3, b, 2, delta_1, target, out_put, out2)
+    delta_2, w2 = delta_rule(w2, w3, b, 1, delta_1, delta_2, lrate, out1, out2)
+    delta_3, w1 = delta_rule(w1, w2, b, 0, delta_2, delta_3, lrate, data, out1)
+    return delta_1, delta_2, delta_3, w1, w2, w3
 
 def train(input_data, target_data, w1, w2, w3, b, lrate, epochs):
-    minimun_error = 10.0
+    minimum_error = 10.0
     comparison_error = 10.0
     for epoch in range(epochs):
         total_error = 0.0
         for i in range(len(input_data)):
-            #순전파
             out_1, out_2, output = Forward_pass(input_data[i], w1, w2, w3, b)
-            #에너지 구하기
             error = 0.0
             for j in range(len(target_data[i])):
                 error += 0.5 * (target_data[i][j] - output[j]) ** 2
             total_error += error
             comparison_error = total_error
-            #역전파
-            Backward_pass(input_data[i], target_data[i], w1, w2, w3, b, out_1, out_2, output, lrate)
-        #학습수가 10000의 배수일때만 출력하자~ 눈 아프다
-        if (epoch % 100 == 0):
+            delta_1, delta_2, delta_3, w1, w2, w3 = Backward_pass(input_data[i], target_data[i], w1, w2, w3, b, out_1, out_2, output, lrate)
+        if epoch % 100 == 0:
             print("step : %4d    Error : %7.4f " % (epoch, total_error))
-        #에러가 전에 나온 에러보다 제일 작을 때만 텍스트 파일에 저장하기
-        if(minimun_error > comparison_error):
-            minimun_error = comparison_error
+        if minimum_error > comparison_error:
+            minimum_error = comparison_error
             weight_write(total_error, w1, w2, w3)
+
 train(input_data, target, weight_in_to_hid1, weight_hid1_to_hid2, weight_hid2_to_out, biases, lrate, epochs)
