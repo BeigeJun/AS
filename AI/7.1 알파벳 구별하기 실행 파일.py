5aa5a280bdaa2e1854a0ef234c54d522795a5bfc
Seo_Jun_ <117076847+BeigeJun@users.noreply.github.com>
2024-01-18 14:36:14 +0900

    Update 7.1 알파벳 구별하기 실행 파일.py

--- AI/7.1 알파벳 구별하기 실행 파일.py
+++ AI/7.1 알파벳 구별하기 실행 파일.py
@@ -19,33 +19,22 @@ a = f.readline()
 weight_hid2_to_out = np.array(a.split(" ")[3:-1])
 weight_hid2_to_out = weight_hid2_to_out.reshape(3,7)
 a = f.readline()
-print(a)
-bias_1 = np.array(a.split(" ")[3:18], dtype=float)
-print(bias_1)
-print(len(bias_1))
-bias_2 = np.array(a.split(" ")[18:25], dtype=float)
-print(bias_2)
-print(len(bias_2))
-bias_3 = np.array(a.split(" ")[25:28], dtype=float)
-print(bias_3)
-print(len(bias_3))
-f.close
+bias_1 = np.array(a.split(" ")[3:18])
+bias_2 = np.array(a.split(" ")[18:25])
+bias_3 = np.array(a.split(" ")[25:28])
+f.close()
+def forward_pass(data, w, b, out):
+    for i in range(len(w)):
+        for number in range(len(w[i])):
+            out[i] += float(w[i][number]) * data[number]
+        out[i] = sigmoid(out[i] + float(b[i]))
+    return out
 def Forward_pass(data, w1, w2, w3, b1,b2,b3):
-    for i in range(len(w1)):  # 15개의 델타값을 만들어야하니까 15번
-        for number in range(len(w1[i])):  # 가중치랑 인풋값 곱학기 25번
-            out_1[i] += float(w1[i][number]) * data[number]
-        out_1[i] = sigmoid(out_1[i] + b1[i])
-    # ---------------------입력층 - > 1차 은닉층--------------------------------------------
-    for i in range(len(w2)):  # 7개의 델타값을 만들어야 하니까 7번
-        for number in range(len(w2[i])):  # 15번의 곱셈을 진행
-            out_2[i] += float(w2[i][number]) * data[number]
-        out_2[i] = sigmoid(out_2[i] + b2[i])
-    # ---------------------1차 은닉층 - > 2차 은닉층----------------------------------------
-    for i in range(len(w3)):  # 3개의 출력을 만들어야 하니까 3번
-        for number in range(len(w3[i])):  # 7번의 곱셈을 진행
-            output[i] += float(w3[i][number]) * out_2[number]
-        output[i] = sigmoid(output[i] + b3[i])
-    # ---------------------2차 은닉층 - > 출력층--------------------------------------------
+    out_1 = forward_pass(data, w1, b1, [0.0] * 15)
+    out_2 = forward_pass(out_1, w2, b2, [0.0] * 7)
+    output = forward_pass(out_2, w3, b3, [0.0, 0.0, 0.0])
+    return output
+
 print("1 :      2:      3 : ")
 print("■■■■■    ■■■■■   ■■■■■")
 print("■□■□□    ■□■□□   □□□□□")
@@ -54,10 +43,10 @@ print("□□■□□    □□■□□   ■□□□□")
 print("□□■□□    □□■□□   ■■■■■")
 print("4 :      5 :     6:")
 print("■■■■■    ■■■■■   ■■■■■")
-print("■□□□□    ■□□□□   ■■■■■")
-print("■■■■■    ■■■■■   ■■■■■")
-print("■□□□■    ■□□□■   ■■■■■")
-print("■■■■■    ■□□■■   ■■■■■")
+print("■□□□□    ■□□□■   ■■■■■")
+print("■■■■□    ■■■■■   ■■■■■")
+print("■□□□□    ■□□□■   ■■■■■")
+print("■■■■■    ■□□□■   ■■■■■")
 input_data = [[1.0, 1.0, 1.0, 1.0, 1.0,
                1.0, 0.1, 1.0, 1.0, 0.1,
                1.0, 0.1, 1.0, 0.1, 0.1,  # T
@@ -89,6 +78,8 @@ input_data = [[1.0, 1.0, 1.0, 1.0, 1.0,
                1.0, 1.0, 1.0, 1.0, 1.0,
                1.0, 1.0, 1.0, 1.0, 1.0]
               ]
-i = int(input("실험할 데이터 선택 : "))
-Forward_pass(input_data[i-1],weight_in_to_hid1,weight_hid1_to_hid2,weight_hid2_to_out,bias_1,bias_2,bias_3)
-print(output)
+
+while True:
+    i = int(input("실험할 데이터 선택 : "))
+    a = Forward_pass(input_data[i-1],weight_in_to_hid1,weight_hid1_to_hid2,weight_hid2_to_out,bias_1,bias_2,bias_3)
+    print(a)
