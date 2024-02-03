06ae4ff33b997c070c60c3549f957a0dbc223746
Seo_Jun_ <117076847+BeigeJun@users.noreply.github.com>
2024-02-04 04:47:58 +0900

    Update 9. 군집화.py

--- AI/9. 군집화.py
+++ AI/9. 군집화.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from copy import deepcopy
 
-Iris_csv = pd.read_csv('C:/Users/SeoJun/PycharmProjects/pythonProject3/DataSet.csv')
+Iris_csv = pd.read_csv('C:/Users/wns20/PycharmProjects/pythonProject10/DataSet.csv')
 SepalLength = Iris_csv.SepalLength.tolist()
 SepalWidth = Iris_csv.SepalWidth.tolist()
 PetalLength = Iris_csv.PetalLength.tolist()
@@ -47,63 +47,64 @@ while True:
 
     x = Iris[int(property_1) - 1]
     y = Iris[int(property_2) - 1]
+    #선택한 속성 복사
     Centroids_x = np.random.uniform(min(x), max(x), k_clusters)
     Centroids_y = np.random.uniform(min(y), max(y), k_clusters)
+    #속성에서 제일 큰 값과 작은 값을 뽑아서 그 사이 값으로 3개의 랜덤좌표 생성(중심점)
     if(Choose_num == '1'):
+        #속성 2개 선택
         Centroids = list(zip(Centroids_x, Centroids_y))
+        #x,y 값을 묶어서 리스트로 변환
         Centroids = np.array(Centroids)
         Centroids_Old = np.zeros(Centroids.shape)
-        Error = np.zeros(k_clusters)
+        #변하는 위치를 저장할 변수 생성
+        Error = np.ones(k_clusters)
+        #데이터와 중심점의 거리 저장(Error가 전부 0 일 때 까지 반복하기 위해 1로 생성)
         Lables = np.zeros(150)
         Input_data_xy = np.array(list(zip(x,y)))
+        #좌표에 찍기위해 선택한 속성 리스트로 변환
+        plt.scatter(x, y, alpha=0.5)
+        plt.scatter(Centroids_x, Centroids_y,alpha=0.5, c='red')
+        plt.show()
+        #좌표에 점찍기
 
-        for i in range(k_clusters):
-            Error[i] = Euclidean_distance(Centroids_Old[i], Centroids[i])
-            print(Centroids[i])
-        Turn = 0
-        while(Error.all != 0):
-            Turn += 1
-            for i in range(k_clusters):
+        while(Error.all() != 0):
+            for i in range(150):
                 Distance = np.zeros(k_clusters)
+                #Distance는 데이터 한 점과 중심점 3개의 거리를 저장해 둘 리스트
                 for j in range(k_clusters):
                     Distance[j] = Euclidean_distance(Input_data_xy[i], Centroids[j])
-                Cluster = Distance.argmin()
-                Lables[i] = Cluster
+                    #거리 구해서 저장하기
+                Lables[i] = Distance.argmin()
+                #거리가 제일 짧은 중심점으로 지정하기
             Centroids_Old = deepcopy(Centroids)
-
+            #중심점을 옮기기 전 복사해 두기
             for i in range(k_clusters):
                 Points = [Input_data_xy[j] for j in range(len(Input_data_xy)) if Lables[j] == i]
+                #리스트 컴프리헨션
+                # Points = []
+                # for j in range(len(Input_data_xy)):
+                #     if Lables[j] == i:
+                #         Points.append(Input_data_xy[j])
                 Centroids[i] = np.mean(Points, axis=0)
-            print(Turn, "번째 군집")
-            for i in range(k_clusters):
-                Error[i] = Euclidean_distance(Centroids_Old[i], Centroids[i])
+
             plt.scatter(x, y, c=Lables, alpha=0.5)
             plt.scatter(Centroids_Old[:, 0], Centroids_Old[:, 1], c='blue')
             plt.scatter(Centroids[:, 0], Centroids[:, 1], c='red')
             plt.show()
+            #중심점이 움직이기 전 후를 보여주고 3개로 나뉜 데이터를 찍어줌
+            for i in range(k_clusters):
+                Error[i] = Euclidean_distance(Centroids_Old[i], Centroids[i])
+            #Error를 계산함으로써 중심점이 바뀌었는지 확인함 이는 While문의 종료에 간여함
         colors = ['r', 'g', 'b']
+
         for i in range(k_clusters):
             Points = np.array([Input_data_xy[j] for j in range(len(Input_data_xy)) if Lables[j] == i])
             plt.scatter(Points[:, 0], Points[:, 1], c=colors[i], alpha=0.5)
             print(colors[i], "의 개수는", len(Points[:, 1]))
+        #끝이나면 r,g,b의 개수를 출력
         plt.scatter(Centroids[:, 0], Centroids[:, 1], marker='D', s=150)
         plt.show()
-        #
-        # plt.scatter(Centroids_x, Centroids_y, s = 50, c = 'red')
-        # #랜덤위치 3개 찍기
-        # plt.scatter(x, y , alpha=0.5, s = 50)
-        # #데이터 찍기
-        # plt.show()
-
-
-
-
-
-
-
-
-
-
 
     elif(Choose_num == '2'):
         z = Iris[int(property_3) - 1]
