bd82d3e6defb965b7efbe287cbeb00a5cd220152
Seo_Jun_ <117076847+BeigeJun@users.noreply.github.com>
2024-02-10 01:43:43 +0900

    Update pytorch연습.py

--- AI/연습/pytorch연습.py
+++ AI/연습/pytorch연습.py
@@ -116,4 +116,107 @@ print(torch.unique(torch.tensor([1,2,3,1,2,2])))
 #inplace 방식으로는 y.add_(x)로 표현이 가능한데 _가 붙는 차이점이 있다 이러면 y = y + x와 같다
 #빼기는 torch.sub로 사용
 
-#38분47초
+x = torch.rand(2, 2)
+y = torch.rand(2, 2)
+
+print(x)
+print(y)
+print(x * y)
+print(torch.mul(x, y))
+
+print(x / y)
+print(torch.div(x, y))
+
+#dot product
+print(torch.matmul(x, y))
+z = torch.mm(x, y)
+print(z)
+#행렬 분해(U,S,V tensor로 분해)
+# print(torch.svd(z))
+
+#인덱싱
+x = torch.Tensor([[1,2],
+                 [3,4]])
+print(x)
+print(x[0, 0])
+print(x[1][1])
+print(x[0, 1])
+print(x[1, 0])
+print(x[1, 1])
+print(x[:, 0])
+print(x[:, 1]) #모든 행에서 1열
+print(x[0, :])
+print(x[1, :])
+
+#view는 텐서의 크기나 모양을 변경하는데 사용 단, 기본적으로 변경 전과 후레 텐서 안의 원소 개수가 유지되어야 함 -1로 설정되면 계산을 통해 해당 크기값을 유추(자동으로 설정)
+x = torch.randn(4,5)
+print(x)
+y = x.view(20)
+print(y)
+z = x.view(5, -1)
+print(z)
+#item: 텐서에 값이 단 하나라도 존재하면 숫자값을 얻을 수 있음 단, 스칼라 값 하나만 존재해야 item()사용 가능
+x = torch.randn(1)
+print(x)
+print(x.item())
+
+#차원을 축소(제거)
+tensor = torch.rand(1,3,3)
+print(tensor)
+print(tensor.shape)
+t=tensor.squeeze()
+print(t)
+print(t.shape)
+#차원을 생성
+tensor = torch.rand(3,3)
+print(tensor)
+print(tensor.shape)
+t=tensor.unsqueeze(dim=0)
+print(t)
+print(t.shape)
+#텐서간 결합 (stack)
+x = torch.FloatTensor([1,4])
+print(x)
+y = torch.FloatTensor([2,5])
+print(y)
+z = torch.FloatTensor([3,6])
+print(z)
+print(torch.stack([x,y,z]))
+#cat : 텐서를 결합하는 메소드
+a = torch.rand(1,3,3)
+print(a)
+b = torch.rand(1,3,3)
+print(b)
+c = torch.cat((a,b),dim=0) #dim을 이용해 몇차원을 이용할지 선택가능
+print(c)
+print(c.size())
+#chunk : 텐서를 여러개로 나눌 때
+tensor = torch.rand(3,6)
+print(tensor)
+t1,t2,t3 = torch.chunk(tensor,3,dim=1)
+print(t1)
+print(t2)
+print(t3)
+#split : chunk와 동일한 기능이지만 다름 chunk는 몇개로 나룰지를 선택하지만 텐서의 크기로 split이 이루어짐
+tensor = torch.rand(3,6)
+print(tensor)
+t1,t2 = torch.split(tensor,3,dim=1)
+print(tensor)
+print(t1)
+print(t2)
+#토치에서 넘파이로 변환
+import numpy as np
+a = torch.ones(7)
+print(a)
+b = a.numpy()
+print(b)
+#텐서가 CPU상에 있으면 넘파이 배열은 메모리 공간을 공유하므로 하나가 변하면 다른 하나도 변함
+a.add_(1)
+print(a)
+print(b)
+
+a = np.ones(7)
+b = torch.from_numpy(a)
+np.add(a,1,out=a)
+print(a)
+print(b)
