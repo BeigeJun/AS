e7502ca4d298455b123275912b089ef31bcaf7f1
Seo_Jun_ <117076847+BeigeJun@users.noreply.github.com>
2024-02-27 00:57:18 +0900

    Update 글자 교정.py

--- AI/연습/글자 교정.py
+++ AI/연습/글자 교정.py
@@ -2,22 +2,23 @@ import torch
 import torch.nn as nn
 import numpy as np
 #입력, 출력값 선언
+def normalize(array):
+    Max = 0.0
+    Min = 10.0
+    for i in range(len(array)):
+        compare_Max = max(array[i])
+        compare_Min = min(array[i])
+        if compare_Max > Max:
+            Max = compare_Max
+        if compare_Min < Min:
+            Min = compare_Min
+    array = (array - Min+0.01) / (Max - Min)
+    return array
+
 data = torch.FloatTensor([[0.12610, 0.12615, 0.01],
-                          [0.12628, 0.12615, 0.01],
+                          [0.12601, 0.12640, 0.01],
                           [0.12599, 0.12615, 0.01]])
-Max = 0.0
-Min = 10.0
-for i in range(len(data)):
-    compare_Max = max(data[i])
-    compare_Min = min(data[i])
-    if compare_Max > Max:
-        Max = compare_Max
-    if compare_Min < Min:
-        Min = compare_Min
-print(Max,Min)
-for i in range(len(data)):
-    for j in range(len(data[i])):
-        data[i][j] = (data[i][j] - Min) / (Max - Min)
+data = normalize(data)
 print(data)
 Output = torch.FloatTensor([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
 #신경망 모델 만들기
@@ -36,7 +37,7 @@ optimizer = torch.optim.SGD(Model.parameters(), lr=0.1)
 #모델의 파라미터를 SGD 옵티마이저를 사용하여 업데이트하기 위한 설정을 생성
 #Model.parameters(): 모델의 파라미터들을 인자로 전달
 #lr : 학습률(learning rate)
-for epoch in range(20000):
+for epoch in range(100000):
     optimizer.zero_grad() #그래디언트 초기화
 
     reselt = Model(data) #순전파
@@ -47,24 +48,12 @@ for epoch in range(20000):
     optimizer.step() #역전파를 통해 얻은 기울기로 가중치 업데이트
     #위 두개는 세트
 
-    if epoch % 100 == 0:
+    if epoch % 1000 == 0:
         print(epoch, Error.item())
-Input = torch.FloatTensor([[0.12610, 0.12631, 0.12615],
-                          [0.12628, 0.12625, 0.01],
-                          [0.12599, 0.12615, 0.01]])
-Max = 0.0
-Min = 10.0
-for i in range(len(Input)):
-    compare_Max = max(Input[i])
-    compare_Min = min(Input[i])
-    if compare_Max > Max:
-        Max = compare_Max
-    if compare_Min < Min:
-        Min = compare_Min
-print(Max,Min)
-for i in range(len(Input)):
-    for j in range(len(Input[i])):
-        Input[i][j] = (Input[i][j] - Min) / (Max - Min)
+Input = torch.FloatTensor([[0.12610, 0.12632,1.0],
+                          [0.12628, 0.12636,1.0],
+                          [0.12599, 0.12615,1.0]])
+Input = normalize(Input)
 Reselt = Model(Input)
 Max = torch.argmax(Reselt, dim=1) + 1
 print('출력값: ', Max.numpy())
