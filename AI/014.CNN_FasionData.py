44c0a4a68a5c276027fbec60c93ad044c3b0081d
Seo_Jun_ <117076847+BeigeJun@users.noreply.github.com>
2024-03-14 15:52:30 +0900

    Create 014.CNN_FasionData.py

---
+++ AI/014.CNN_FasionData.py
@@ -0,0 +1,67 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, transforms
+import torch.nn.functional as F
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+train_dataset = datasets.FashionMNIST(root='./data', train=True, download=True, transform=transform)
+test_dataset = datasets.FashionMNIST(root='./data', train=False, download=True, transform=transform)
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False)
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=20, kernel_size=5, stride=1)
+        self.conv2 = nn.Conv2d(in_channels=20, out_channels=50, kernel_size=5, stride=1)
+        self.fc1 = nn.Linear(4 * 4 * 50, 500)
+        self.fc2 = nn.Linear(500, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+
+        x = x.view(-1, 4 * 4 * 50)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+cnn = CNN()
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = optim.SGD(cnn.parameters(), lr=0.01)
+
+cnn.train()
+for epoch in range(10):
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):  # enumerate는 train_loader에 있는 순서와 데이터를 뽑아오는 기능을 한다.
+        inputs, labels = data
+        optimizer.zero_grad()
+
+        outputs = cnn(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % 100 == 99:
+            print('[%d, %d] loss: %.5f' % (epoch + 1, i + 1, running_loss / 100))
+            running_loss = 0.0
+cnn.eval()  # test case 학습 방지를 위함
+test_loss = 0
+correct = 0
+with torch.no_grad():
+  for data, target in test_loader:
+    output = cnn(data)
+    test_loss += criterion(output, target).item()
+    pred = output.argmax(dim=1, keepdim=True)
+    correct += pred.eq(target.view_as(pred)).sum().item()
+print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
