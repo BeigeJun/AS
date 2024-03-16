50d31338aa8786b3f910d926061cff3cf7cb98cf
Seo_Jun_ <117076847+BeigeJun@users.noreply.github.com>
2024-03-17 02:47:59 +0900

    Update 013.CNN_MNIST.py

--- AI/013.CNN_MNIST.py
+++ AI/013.CNN_MNIST.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.1307,), (0.3081,))
@@ -12,7 +13,7 @@ transform = transforms.Compose([
 train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
 test_dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=100, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=100, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=100, shuffle=False)
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
@@ -37,13 +38,14 @@ criterion = torch.nn.CrossEntropyLoss()
 optimizer = optim.SGD(cnn.parameters(), lr=0.01)
 
 cnn.train()
-for epoch in range(10):
+for epoch in range(5):
     running_loss = 0.0
-    for i, data in enumerate(train_loader, 0):  # enumerate는 train_loader에 있는 순서와 데이터를 뽑아오는 기능을 한다.
+    for i, data in enumerate(train_loader, 0):
         inputs, labels = data
         optimizer.zero_grad()
 
         outputs = cnn(inputs)
+
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
@@ -52,15 +54,47 @@ for epoch in range(10):
         if i % 100 == 99:
             print('[%d, %d] loss: %.5f' % (epoch + 1, i + 1, running_loss / 100))
             running_loss = 0.0
+
+
 cnn.eval()  # test case 학습 방지를 위함
 test_loss = 0
 correct = 0
 with torch.no_grad():
-  for data, target in test_loader:
-    output = cnn(data)
-    test_loss += criterion(output, target).item()
-    pred = output.argmax(dim=1, keepdim=True)
-    correct += pred.eq(target.view_as(pred)).sum().item()
+    data_iter = iter(test_loader)
+    images, labels = next(data_iter)
+    First_conv_outputs = F.relu(cnn.conv1(images))
+    First_pool_outputs = F.max_pool2d(First_conv_outputs, kernel_size=2, stride=2)
+    Second_conv_outputs = F.relu(cnn.conv2(First_pool_outputs))
+    Second_pool_outputs = F.max_pool2d(Second_conv_outputs, kernel_size=2, stride=2)
+    plt.imshow(images[0][0].detach().numpy(), cmap='gray')
+    plt.title("Original Image")
+    plt.show()
+    #앞이 배치 순서, 뒤가 배치에서의 순서
+    for i in range(20):
+        plt.subplot(5, 4, i + 1)
+        plt.imshow(First_conv_outputs[0][i].squeeze().detach().numpy(), cmap='gray')
+        plt.title("First Convolution Output")
+    plt.show()
+    for i in range(20):
+        plt.subplot(5, 4, i + 1)
+        plt.imshow(First_pool_outputs[0][i].squeeze().detach().numpy(), cmap='gray')
+        plt.title("First Pooling Output")
+    plt.show()
+    for i in range(50):
+        plt.subplot(10, 5, i + 1)
+        plt.imshow(Second_conv_outputs[0][i].squeeze().detach().numpy(), cmap='gray')
+        plt.title("Second Convolution Output")
+    plt.show()
+    for i in range(50):
+        plt.subplot(10, 5, i + 1)
+        plt.imshow(Second_pool_outputs[0][i].squeeze().detach().numpy(), cmap='gray')
+        plt.title("Second Pooling Output")
+    plt.show()
+    for data, target in test_loader:
+       output = cnn(data)
+       test_loss += criterion(output, target).item()
+       pred = output.argmax(dim=1, keepdim=True)
+       correct += pred.eq(target.view_as(pred)).sum().item()
 print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
